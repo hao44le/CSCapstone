07264cb519fa547df56cecefb2a18a7f9781f251
Frostao <chawk9621@gmail.com>
2016-12-05 17:58:34 -0500

    comment update

--- CommentsApp/models.py
+++ CommentsApp/models.py
@@ -8,7 +8,7 @@ from AuthenticationApp.models import MyUser
 class Comment(models.Model):
     time = models.DateTimeField(auto_now=True)
     comment = models.CharField(max_length=500)
-    group = models.OneToOneField(Group,default=None,null=True)
-    createdBy = models.OneToOneField(MyUser,default=None,null=True)
+    group = models.ForeignKey(Group,default=None,null=True)
+    createdBy = models.ForeignKey(MyUser,default=None,null=True)
     def __str__(self):
         return self.comment
--- GroupsApp/views.py
+++ GroupsApp/views.py
@@ -24,6 +24,28 @@ def getGroups(request):
 
 def getGroup(request):
     if request.user.is_authenticated():
+        if request.method == 'POST':
+            form = forms.CommentForm(request.POST)
+            if form.is_valid():
+                in_name = request.POST.get('name', 'None')
+                print request.body
+                in_group = models.Group.objects.get(name__exact=in_name)
+                new_comment = models.Comment(comment=form.cleaned_data['comment'],createdBy=request.user,group=in_group)
+                new_comment.save()
+                is_member = in_group.members.filter(email__exact=request.user.email)
+                is_student = request.user.is_student
+                comments_list = Comment.objects.all()
+                projects = in_group.project.all()
+                context = {
+                    'group': in_group,
+                    'userIsMember': is_member,
+                    'comments' : comments_list,
+                    'is_student': is_student,
+                    'projects' : projects
+                }
+                return render(request, 'group.html', context)
+            else:
+                form = forms.CommentForm()
         in_name = request.GET.get('name', 'None')
         in_group = models.Group.objects.get(name__exact=in_name)
         is_member = in_group.members.filter(email__exact=request.user.email)
@@ -134,3 +156,5 @@ def deleteGroup(request):
         return render(request, 'groups.html', context)
     # render error page if user is not logged in
     return render(request, 'autherror.html')
+
+
